b5f5e6d30c833accdcf0b57ebdab21a8edc04b23
k31p <108131875+k31p@users.noreply.github.com>
2022-06-30 15:40:00 +0700

    added footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,5 +11,6 @@
             </div>
         </div>
     </div>
+    @include('layout.footer')
 </body>
 @endsection
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="col-12 position-absolute bottom-0 bg-dark">
+    <h6 class="text-center">Copyright&copy PT. RoyAir Enterprise | All Rights Reserved</h6>
+</div>
+
